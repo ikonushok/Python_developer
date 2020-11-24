0de2fba5a9748122dde5427a8ccf4e64b8539233
ikonushok <ikonushok@gmail.com>
2020-11-24 12:17:48 +0300

    получение курса с сайта ЦБР

---
+++ cbr.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+response = requests.get(url)
+data = json.loads(response.text)
+print(data)
+
+print(f"Курс {data['Valute']['GBP']['Name']} - {data['Valute']['GBP']['Value']}")
