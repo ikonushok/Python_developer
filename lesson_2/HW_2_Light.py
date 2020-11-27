56658f195ec88cc060f77908f008401c05a67b8a
ikonushok <ikonushok@gmail.com>
2020-11-27 12:22:15 +0300

    lesson 2

---
+++ lesson_2/HW_2_Light.py
@@ -0,0 +1,23 @@
+print('Задача 1:')
+for i in range(1, 6): print (i, '0'*10)
+
+print('\nЗадача 2:')
+#n = 0
+#for _ in range(10):
+#    a = input('Введите число: ')
+#    if a == '5': n += 1
+#print('Введено ', n, ' чисел 5')
+
+print('\nЗадача 3:')
+n = 0
+for i in range (1, 101): n += i
+print ('Сумма числе от 1 до 100 = ', n)
+
+print('\nЗадача 4:')
+n = 1
+for i in range (1, 11): n *= i
+print ('Произведение числе от 1 до 10 = ', n)
+
+print('\nЗадача 5:')
+a = input('Введите больше число: ')
+for i in a: print (i)
