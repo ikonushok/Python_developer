f162529aa791cdfc1b2d0f1d82a84e85224669b6
ikonushok <ikonushok@gmail.com>
2020-12-09 15:46:12 +0300

    lesson 3

---
+++ lesson_4/HW_4_UltraLight.py
@@ -0,0 +1,14 @@
+# Функция без параметров
+def empty():
+    print('Это функция без параметров')
+
+print(empty())
+
+# Функция с параметрами
+def sqr(a):
+    return a**2
+print(sqr(4))
+
+# Лямбда функция
+s = lambda x: x**3
+print(s(3))
