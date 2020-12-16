33956b26f2431ab49e65f598dd152728638582b4
ikonushok <ikonushok@gmail.com>
2020-12-16 15:04:17 +0300

    lesson 5

---
+++ lesson_5/HW_5_pro.py
@@ -0,0 +1,23 @@
+from divisor_master import dividers_num
+
+num = int(input('Введите число: '))
+
+def list_simple_dividers(num):
+    prime_list = dividers_num(num)[1:]
+    print(prime_list)
+    result = []
+
+    for i in prime_list:
+        while num % i == 0:
+            result.append(i)
+            num //= i
+        if num == 1:
+            break
+    return result
+
+print('Каноническое разложение числа: ', list_simple_dividers(num))
+
+def divider_max(num):
+    return max(dividers_num(num))
+
+print('Самый большой делитель: ', divider_max(num) )
